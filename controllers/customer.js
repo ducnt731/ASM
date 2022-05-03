bb6748932f139ce466b2fd6f8f6e2c13b7660739
Thanh <thanhvkgdh190702@fpt.edu.vn>
2022-05-04 00:02:26 +0700

    Customer cancel order

--- controllers/customer.js
+++ controllers/customer.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const dbHandler = require('../databaseHandler');
+const { ObjectId } = require('mongodb')
 const {getHistory,getOder,insertObject,USER_TABLE_NAME, getAllDocumentsFromCollection, deleteDocumentById, updateCollection, getDocumentById,getCustomer} = require('../databaseHandler')
 
 
@@ -137,11 +138,14 @@ router.get('/deletemyorder', async (req, res) => {
   res.redirect('purchasehistory')// return viewprofile page
 })
 
-
-router.post('/cancelmyorder',async (req,res) =>{
-  const statusInput = req.body.txtstatus
+router.get('/cancelmyorder',async (req,res) =>{
+  console.log("2")
+  const statusInput =  "Cancel"
   //ham update
-  const id = req.body.txtId
+  console.log("3")
+  console.log(statusInput)
+  const id = req.query.id
+  console.log(id)
   const myquery = { _id: ObjectId(id) }
   const newvalues = {$set: {
       status: statusInput,
@@ -152,7 +156,7 @@ router.post('/cancelmyorder',async (req,res) =>{
   console.log(id)
   
   const collectionName = "Order"
-  await updateCollection(collectionName, myquery, newvalues)
+  await updateCollection(collectionName, myquery, newvalues, )
   res.redirect('purchasehistory')
 })
 
